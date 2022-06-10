80e86cea8fd99b87cb5da215e5fdd2fd82535736
Illya <pelykh.ilya@gmail.com>
2022-06-10 08:14:38 +0300

    class

--- Лекции/js/StartLearning2.js
+++ Лекции/js/StartLearning2.js
@@ -180,3 +180,36 @@
 
 // const double = a => a * 2;
 // console.log(double(5));
+
+// // ---------------------------Классы---------------------------
+
+// class Rectangle {
+//     constructor(height, width) {
+//         this.height = height;
+//         this.width = width;
+//     }
+
+//     calcArea() {
+//         return this.height * this.width;
+//     }
+// }
+
+// const square = new Rectangle(5, 5);
+// console.log(square.calcArea());
+
+// //Наследование (extends). super - вызывает конструктор родителя
+// class ColoreRectangeWithText extends Rectangle {
+//     constructor(height, width, text, bgColor) {
+//         super(height, width);
+//         this.text =text;
+//         this.bgColor = bgColor;
+//     }
+
+//     showMyProps() {
+//         console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+//     }
+// }
+
+// const div = new ColoreRectangeWithText(10, 20, 'Hello', 'yelow');
+// console.log(div.calcArea());
+// div.showMyProps();
