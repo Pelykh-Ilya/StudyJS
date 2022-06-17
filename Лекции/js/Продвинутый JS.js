55e50ca9198e9985a4a5f56ae69c398704ab9b7f
Illya <pelykh.ilya@gmail.com>
2022-06-17 06:43:59 +0300

    JSON calc

---
+++ Лекции/js/Продвинутый JS.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// //----------------------------JSON----------------------------
+
+// const persone = {
+//     name: 'Alex',
+//     tel: '+4777777',
+//     parents: {
+//         mom: 'Olga',
+//         dad: 'Mike'
+//     }
+// };
+
+// console.log(JSON.stringify(persone));
+
+// const jsonSTR = JSON.stringify(persone);
+// console.log(JSON.parse(jsonSTR));
+
+// // Глубокая копия
+// const clone = JSON.parse(JSON.stringify(persone));
+// console.log(clone);
+// clone.parents.mom = 'Tanya';
+// console.log(persone);
+// console.log(clone);
