a6e1d7ec6b5faa8912de915b9a29f299c7ec402d
Illya <pelykh.ilya@gmail.com>
2022-06-19 09:06:30 +0300

    promise

--- Лекции/js/Продвинутый JS.js
+++ Лекции/js/Продвинутый JS.js
@@ -22,3 +22,58 @@
 // clone.parents.mom = 'Tanya';
 // console.log(persone);
 // console.log(clone);
+
+
+//----------------------------Promise----------------------------
+
+// console.log('Запрос данных...');
+
+// const req = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Подготовка данных...');
+
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         };
+//         resolve(product);
+//     }, 2000);
+// });
+
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'ordered';
+//             resolve(product);
+//             // reject();
+//         }, 2000);
+//     });
+// }).then(data => {
+//     data.modify = true;
+//     return data;
+// }).then((data) => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error('Произошла ошибка');
+// }).finally(() => {
+//     console.log('Finally');
+// });
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    });
+};
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all ждет выполнения всех промисов и только после этого выводит результат
+Promise.all([test(1000), test(2000)]).then(() => {
+    console.log('ALL');
+});
+
+// Promise.race выполняеться когда самы первый промис сработал
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('Race');
+});
