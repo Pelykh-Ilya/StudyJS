ae65fe7acb5bed46808d22eb65abbc939c5219bd
Illya <pelykh.ilya@gmail.com>
2022-07-08 09:04:13 +0300

    RegExp

--- Лекции/js/Продвинутый JS.js
+++ Лекции/js/Продвинутый JS.js
@@ -78,54 +78,86 @@
 //     console.log('Race');
 // });
 
-//----------------------------Методы перебора массивов----------------------------
+// //----------------------------Методы перебора массивов----------------------------
 
-// filter
+// // filter
 
-const names = ['Ivan', 'Anna', 'Ksenia', 'Voldemart'];
+// const names = ['Ivan', 'Anna', 'Ksenia', 'Voldemart'];
 
-const shortNames = names.filter(function(name) {
-    return name.length < 5;
-});
+// const shortNames = names.filter(function(name) {
+//     return name.length < 5;
+// });
 
-console.log(shortNames);
+// console.log(shortNames);
 
-// map
+// // map
 
-const answers = ['IvAn', 'AnnA', 'Hello'];
+// const answers = ['IvAn', 'AnnA', 'Hello'];
 
-const result = answers.map(item => item.toLowerCase());
+// const result = answers.map(item => item.toLowerCase());
 
-console.log(result);
+// console.log(result);
 
-// every/some(возвращает true или false)
+// // every/some(возвращает true или false)
 
-const some = [4, 'qwq', 'stewrgs'];
+// const some = [4, 'qwq', 'stewrgs'];
 
-console.log(some.some(item => typeof(item) === "number"));
-console.log(some.every(item => typeof (item) === "number"));
+// console.log(some.some(item => typeof(item) === "number"));
+// console.log(some.every(item => typeof (item) === "number"));
 
-// reduce (собирать массив в одно единое целое)
+// // reduce (собирать массив в одно единое целое)
 
-const arr = [4, 5, 1, 3, 2, 6];
-// после функции через запятую можно передать начальное значение
-const res = arr.reduce((sum, current) => sum + current, 21);
-console.log(res);
+// const arr = [4, 5, 1, 3, 2, 6];
+// // после функции через запятую можно передать начальное значение
+// const res = arr.reduce((sum, current) => sum + current, 21);
+// console.log(res);
 
-const arr2 = ['apple', 'pear', 'plum'];
+// const arr2 = ['apple', 'pear', 'plum'];
 
-const res2 = arr2.reduce((sum, curent) => `${sum}, ${curent}`, 'orange');
-console.log(res2);
+// const res2 = arr2.reduce((sum, curent) => `${sum}, ${curent}`, 'orange');
+// console.log(res2);
 
-const obj = {
-    ivan: 'persone',
-    ann: 'persone',
-    dog: 'animal',
-    cat: 'animal'
-};
+// const obj = {
+//     ivan: 'persone',
+//     ann: 'persone',
+//     dog: 'animal',
+//     cat: 'animal'
+// };
 
-const newArr = Object.entries(obj)
-.filter(item => item[1] == 'persone')
-.map(item => item[0]);
+// const newArr = Object.entries(obj)
+// .filter(item => item[1] == 'persone')
+// .map(item => item[0]);
 
-console.log(newArr);
+// console.log(newArr);
+
+
+// Регулярные выражения
+
+// new RegExp('pattern', 'flags');
+// /pattern/flags;
+
+// const ans = prompt('Enter your number');
+
+// const reg = /n/ig;
+// console.log(reg.test(ans));
+
+// const reg = /\d/g;
+// console.log(ans.match(reg));
+
+
+// флаги : i - вне зависимости от регистра; g - найти несколько фхождений; m - многострочный режим;
+// классы : \d - цифры; \w - слова; \s - пробелы
+// обратные классы: \D - не числа; \W - не буквы;
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Enter your password');
+
+// console.log(pass.replace(/\./g, '*'));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
+// const str = 'My name is R2D2';
+
+// console.log(str.match(/\w\d\w\d/i));
